c9ee7ab06705503d82325090039b66dfb7ce4b08
kuldeepj14 <jainkuldeep1407@gmail.com>
2021-06-25 01:34:04 +0530

    account details bug resolved

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.index, name="account"),
-    # path('edit-account', views.edit_account, name="edit-account" ),
+    path('edit-details', views.edit_account_details, name="edit-account-details" ),
     path('password-reset/', views.password_reset, name="account-password-reset" )
 ]
--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.http import request
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from .models import UserAccount
@@ -11,41 +12,49 @@ from django.contrib import messages
 
 
 def index(request):
-    
-
     current_user = User.objects.get(username=request.user)
     exists = UserAccount.objects.filter(user=request.user).exists()
     if not exists:
         UserAccount.objects.create(user =request.user, phone_number='')
+        
     if exists:
         account_details = UserAccount.objects.get(user=request.user)
-        if request.method == "POST":
-    
-            fisrt_name = request.POST['first_name']
-            last_name = request.POST['last_name']
-            birth_date = request.POST['birth_date']
-            email = request.POST['email']
-            phone_number = request.POST['phone_number']
-            
-            current_user.first_name = fisrt_name
-            current_user.last_name = last_name
-            current_user.email = email
-            account_details.phone_number = phone_number
-            account_details.birth_date = birth_date
-            current_user.save()
-            account_details.save()
-        
         context = {
             'current_user' : current_user,
             'account_details' : account_details
         }
+    return render(request,'account/account-details.html', context)
+    
+def edit_account_details(request):
+    current_user = User.objects.get(username=request.user)
+    account_details = UserAccount.objects.get(user=request.user)
+    context = {
+        'current_user' : current_user,
+        'account_details' : account_details
+    }
+    if request.method == "GET":
+        return render(request, 'account/edit-account-details-tab.html', context)
+    if request.method == "POST":
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        birth_date = request.POST['birth_date']
+        email = request.POST['email']
+        phone_number = request.POST['phone_number']
         
-        return render(request,'account/edit-account-details-tab.html', context)
-
+        current_user.first_name = first_name
+        current_user.last_name = last_name
+        current_user.email = email
+        account_details.phone_number = phone_number
+        account_details.birth_date = birth_date
+        
+        current_user.save()
+        account_details.save()
+        messages.success(request, 'details updated successfully.')
+        return redirect('account')
+    return render(request, 'account/edit-account-details-tab.html', context)
 
 def password_reset(request):
-    # view = RequestPasswordResetEmail.as_view()
-    # return view(request)
+
     if request.method == "GET":
         return render(request,'account/password-reset-tab.html')
     if request.method == "POST":
